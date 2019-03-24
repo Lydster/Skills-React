fac46ab9cf576fc0db8490716bd068ad48a9e71d
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-23 21:01:29 -0700

    Fixed random bug

--- src/components/Prisoner.js
+++ src/components/Prisoner.js
@@ -6,143 +6,149 @@ import { Button } from "react-materialize";
 import { Card, Input, Icon, Col } from "react-materialize";
 import Axios from "axios";
 class Prisoner extends React.Component {
-  state = {
-    isEditing: false,
-    prisoner: {
-      name: this.props.prisoners.name,
-      id_number: this.props.prisoners.id_number,
-      skills: []
-    }
-  };
+	state = {
+		isEditing: false,
+		prisoner: {
+			name: this.props.prisoners.name,
+			id_number: this.props.prisoners.id_number,
+			skills: []
+		}
+	};
 
-  componentWillMount() {
-    Axios.get(
-      `https://pskills.herokuapp.com/api/prisoners/${
-        this.props.prisoners.prison_id
-      }`
-    )
-      .then(res => {
-        console.log(res.data);
-        this.setState({
-          prisoner: { skills: res.data.skills }
-        });
-        console.log(res.data.skills);
-      })
-      .catch(err => console.log(err));
-  }
+	componentWillMount() {
+		Axios.get(`https://pskills.herokuapp.com/api/skills`)
+			.then(res => {
+				const filtered = res.data.filter(
+					skill => skill.prisoner_id === this.props.prisoners.id
+				);
 
-  edit = () => {
-    this.setState({
-      isEditing: !this.state.isEditing
-    });
-  };
+				this.setState({
+					prisoner: {
+						...this.state.prisoner,
+						skills: filtered
+					}
+				});
+			})
+			.catch(err => console.log(err));
+	}
 
-  changeHandler = e => {
-    e.persist();
-    this.setState({
-      prisoner: { ...this.state.prisoner, [e.target.name]: e.target.value }
-    });
-  };
+	edit = () => {
+		this.setState({
+			isEditing: !this.state.isEditing
+		});
+	};
 
-  deletePrisoner = e => {
-    e.preventDefault();
-    this.props.deletePrisoner(this.props.prisoners.id);
-  };
+	changeHandler = e => {
+		e.persist();
+		this.setState({
+			prisoner: {
+				...this.state.prisoner,
+				[e.target.name]: e.target.value
+			}
+		});
+	};
 
-  updatePrisoner = e => {
-    e.preventDefault();
-    this.props.updatePrisoner(this.props.prisoners.id, this.state.prisoner);
-    this.setState({
-      isEditing: !this.state.isEditing
-    });
-  };
+	deletePrisoner = e => {
+		e.preventDefault();
+		this.props.deletePrisoner(this.props.prisoners.id);
+	};
 
-  handleAddSkill = skill => {
-    console.log(skill);
-    Axios.post("https://pskills.herokuapp.com/api/skills", skill)
-      .then(res =>
-        this.setState({
-          prisoner: {
-            ...this.state.prisoner,
-            skills: [...this.state.prisoner.skills, res.data]
-          }
-        })
-      )
-      .catch(err => console.log(err));
-  };
+	updatePrisoner = e => {
+		e.preventDefault();
+		this.props.updatePrisoner(this.props.prisoners.id, this.state.prisoner);
+		this.setState({
+			isEditing: !this.state.isEditing
+		});
+	};
 
-  handleDeleteSkill = id => {
-    Axios.delete(`https://pskills.herokuapp.com/api/skills/${id}`)
-      .then(res => {
-        const filtered = this.state.prisoner.skills.filter(skill => {
-          console.log(id);
-          console.log(skill.id);
-          return id !== skill.id;
-        });
-        this.setState({
-          ...this.state,
-          prisoner: { ...this.state.prisoner, skills: filtered }
-        });
-      })
-      .catch(err => console.log(err));
-  };
+	handleAddSkill = skill => {
+		console.log(skill);
+		Axios.post("https://pskills.herokuapp.com/api/skills", skill)
+			.then(res =>
+				this.setState({
+					prisoner: {
+						...this.state.prisoner,
+						skills: [...this.state.prisoner.skills, res.data]
+					}
+				})
+			)
+			.catch(err => console.log(err));
+	};
 
-  render() {
-    console.log(this.state.prisoner.skills);
-    return (
-      <div className="prisoner-cards">
-        <div className="prisoner-card">
-          <Card className="blue-grey darken-1">
-            {!this.state.isEditing ? (
-              <div className="prisoner-text">
-                <h3>{this.state.prisoner.name}</h3>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgyX2YvrpAXbXLAkffPL_0t9P_U8JbtTc5OU6lEINTkhSPnFXW" />
+	handleDeleteSkill = id => {
+		Axios.delete(`https://pskills.herokuapp.com/api/skills/${id}`)
+			.then(res => {
+				const filtered = this.state.prisoner.skills.filter(skill => {
+					console.log(id);
+					console.log(skill.id);
+					return id !== skill.id;
+				});
+				this.setState({
+					...this.state,
+					prisoner: { ...this.state.prisoner, skills: filtered }
+				});
+			})
+			.catch(err => console.log(err));
+	};
 
-                <SkillsList
-                  skills={this.state.prisoner.skills}
-                  handleDeleteSkill={this.handleDeleteSkill}
-                  prisonerId={this.props.prisoners.prison_id}
-                  handleAddSkill={this.handleAddSkill}
-                />
+	render() {
+		console.log(this.state.prisoner.skills);
+		return (
+			<div className="prisoner-cards">
+				<div className="prisoner-card">
+					<Card className="blue-grey darken-1">
+						{!this.state.isEditing ? (
+							<div className="prisoner-text">
+								<h3>{this.state.prisoner.name}</h3>
+								<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgyX2YvrpAXbXLAkffPL_0t9P_U8JbtTc5OU6lEINTkhSPnFXW" />
 
-                {this.props.match.url === "/private" ? (
-                  <span>
-                    <Button onClick={this.deletePrisoner}>Delete</Button>
-                    <Button onClick={this.edit}>
-                      Edit<Icon>create</Icon>
-                    </Button>
-                  </span>
-                ) : null}
-              </div>
-            ) : (
-              <div className="prisoner-text">
-                <h2>Edit a Prisoner</h2>
-                <form onSubmit={this.updatePrisoner}>
-                  <Input
-                    type="text"
-                    name="name"
-                    placeholder="name"
-                    value={this.state.prisoner.name}
-                    onChange={this.changeHandler}
-                  />
-                  <Input
-                    type="text"
-                    name="id_number"
-                    placeholder="id_number"
-                    value={this.state.prisoner.id_number}
-                    onChange={this.changeHandler}
-                  />
+								<SkillsList
+									skills={this.state.prisoner.skills}
+									handleDeleteSkill={this.handleDeleteSkill}
+									prisonerId={this.props.prisoners.prison_id}
+									handleAddSkill={this.handleAddSkill}
+								/>
 
-                  <Button>update</Button>
-                  <Button onClick={this.edit}>cancel</Button>
-                </form>
-              </div>
-            )}
-          </Card>
-        </div>
-      </div>
-    );
-  }
+								{this.props.match.url === "/private" ? (
+									<span>
+										<Button onClick={this.deletePrisoner}>
+											Delete
+										</Button>
+										<Button onClick={this.edit}>
+											Edit<Icon>create</Icon>
+										</Button>
+									</span>
+								) : null}
+							</div>
+						) : (
+							<div className="prisoner-text">
+								<h2>Edit a Prisoner</h2>
+								<form onSubmit={this.updatePrisoner}>
+									<Input
+										type="text"
+										name="name"
+										placeholder="name"
+										value={this.state.prisoner.name}
+										onChange={this.changeHandler}
+									/>
+									<Input
+										type="text"
+										name="id_number"
+										placeholder="id_number"
+										value={this.state.prisoner.id_number}
+										onChange={this.changeHandler}
+									/>
+
+									<Button>update</Button>
+									<Button onClick={this.edit}>cancel</Button>
+								</form>
+							</div>
+						)}
+					</Card>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Prisoner;
--- src/components/SkillsList.js
+++ src/components/SkillsList.js
@@ -3,55 +3,55 @@ import Skill from "./Skill";
 import { Button } from "react-materialize";
 
 class SkillsList extends React.Component {
-  state = {
-    skill: {
-      name: "",
-      prisoner_id: this.props.prisonerId
-    }
-  };
+	state = {
+		skill: {
+			name: "",
+			prisoner_id: this.props.prisonerId
+		}
+	};
 
-  addSkill = e => {
-    e.preventDefault();
-    this.props.handleAddSkill(this.state.skill);
-    this.setState({
-      ...this.state.skill,
-      name: ""
-    });
-  };
+	addSkill = e => {
+		e.preventDefault();
+		this.props.handleAddSkill(this.state.skill);
+		this.setState({
+			...this.state.skill,
+			name: ""
+		});
+	};
 
-  changeHandler = e => {
-    e.persist();
-    this.setState({
-      skill: { ...this.state.skill, [e.target.name]: e.target.value }
-    });
-  };
+	changeHandler = e => {
+		e.persist();
+		this.setState({
+			skill: { ...this.state.skill, [e.target.name]: e.target.value }
+		});
+	};
 
-  render() {
-    console.log(this.props);
-    return (
-      <div>
-        {this.props.skills.map(skill => {
-          return (
-            <Skill
-              {...skill}
-              handleDeleteSkill={this.props.handleDeleteSkill}
-              key={this.props.prisonerId}
-              id={this.props.prisonerId}
-            />
-          );
-        })}
-        <form onSubmit={this.addSkill}>
-          <input
-            value={this.state.skill.name}
-            placeholder="add skill"
-            onChange={this.changeHandler}
-            name="name"
-          />
-          <Button>Add</Button>
-        </form>
-      </div>
-    );
-  }
+	render() {
+		console.log(this.props);
+		return (
+			<div>
+				{this.props.skills.map(skill => {
+					return (
+						<Skill
+							{...skill}
+							handleDeleteSkill={this.props.handleDeleteSkill}
+							key={skill.id}
+						/>
+					);
+				})}
+
+				<form onSubmit={this.addSkill}>
+					<input
+						value={this.state.skill.name}
+						placeholder="add skill"
+						onChange={this.changeHandler}
+						name="name"
+					/>
+					<Button>Add</Button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default SkillsList;
